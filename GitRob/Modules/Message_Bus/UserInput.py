f240df996b833cc4b09285bdc6a98c2b78e33ec8
Murali M <mvpmurali@gmail.com>
2017-11-24 16:32:23 +0530

    Updated

---
+++ GitRob/Modules/Message_Bus/UserInput.py
@@ -0,0 +1,9 @@
+class UserInput(object):
+
+    ROBOT_LIBRARY_VERSION = 1.0
+
+    def __init__(self):
+        pass
+
+    def keyword(self):
+        pass
---
+++ GitRob/ReusableLibraries/coreFunctionsUI.py
@@ -0,0 +1,9 @@
+class coreFunction(object):
+
+    ROBOT_LIBRARY_VERSION = 1.0
+
+    def __init__(self):
+        pass
+
+    def keyword(self):
+        pass
